f0f6fa1918a9e9791d92bac603e45a108d00164e
Leo Lee <nxl1969@gmail.com>
2017-02-19 16:56:47 -0500

    Remove some comments

--- php/getLRD.php
+++ php/getLRD.php
@@ -3,10 +3,9 @@
 	{
 		$contents = file_get_contents("https://maps.google.com/?cid=".$_GET['place_cid']);
 		
-		//echo $contents;
+		
 		preg_match("/(0x[a-z0-9]{4,}:0x[a-z0-9]{4,})/",$contents,$matches);
-		//preg_match("/^ludocid=(*)\,$/",$contents,$matches);
-		//var_dump($matches);
+		
 		echo $matches[0];
 		
 	}

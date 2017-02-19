4de2e7efb995645bae6fb9ba172db51402f79dff
Leo Lee <nxl1969@gmail.com>
2017-02-19 17:06:20 -0500

    Add some css styles Display "Generating links" message before showing the links

--- src/components/LinkOptions.js
+++ src/components/LinkOptions.js
@@ -4,11 +4,18 @@ import {connect} from 'react-redux';
 class LinkOptions extends Component{
 	
 	render(){
-		console.log(this.props.LRD);
+		
+		console.log("isFetching:",this.props.isFetching);
 		if(!this.props.LRD)
 		{
+			if(this.props.isFetching)
+			{
+				return <div>Generating links...Please wait...</div>
+			}
 			return <div></div>;
 		}
+		
+			
 		const LRD = this.props.LRD;
 		const place = this.props.biz;
 		const biz_name = place.name;
@@ -18,9 +25,7 @@ class LinkOptions extends Component{
 		const ROOT_URL = "https://www.google.com/search?q=";
 		
 								
-							    /*var q2 = escape($("#biz_name").text().replace(/\s{2,}/g,' ').replace(/ /g,'+'))+"+"+street.replace(/ /g,'+')+"+"+city.replace(/ /g,'+')+"+"+state+"&fpstate=lurev_"+data+"&lrd="+data+",3,5";
-								
-							    var q3 = escape($("#biz_name").text().replace(/\s{2,}/g,' ').replace(/ /g,'+'))+"+"+street.replace(/ /g,'+')+"+"+city.replace(/ /g,'+')+"+"+state+"&fpstate=lurev_"+data;*/
+							   
 								
 		const query_str1 = biz_query + "&lrd="+LRD+",3,5&rlimm="+place_cid+"&istate=lrl:iv";
 		const query_str2 = biz_query + "&fpstate=lurev_"+LRD+"&lrd="+LRD+",3,5";
@@ -29,15 +34,18 @@ class LinkOptions extends Component{
 			<div id="linkOptions">
 				<div className="input-group">
 					<span className="input-group-addon" id="basic-addon1">Option1</span>
-					<input type="text" className="form-control" id="option1" aria-describedby="basic-addon1" readOnly value={ROOT_URL+query_str1} />
+					<input type="text" className="form-control link-option" id="option1" aria-describedby="basic-addon1" readOnly value={ROOT_URL+query_str1} />
+					<span className="input-group-btn"><a href={ROOT_URL+query_str1} target="_blank" className="btn btn-secondary">Try it</a></span>
 				</div>
 				<div className="input-group">
 					<span className="input-group-addon" id="basic-addon2">Option2</span>
-					<input type="text" className="form-control" id="option2" aria-describedby="basic-addon2" readOnly value={ROOT_URL+query_str2} />
+					<input type="text" className="form-control link-option" id="option2" aria-describedby="basic-addon2" readOnly value={ROOT_URL+query_str2} />
+					<span className="input-group-btn"><a href={ROOT_URL+query_str2} target="_blank" className="btn btn-secondary">Try it</a></span>
 				</div>
 				<div className="input-group">
 					<span className="input-group-addon" id="basic-addon3">Option3</span>
-					<input type="text" className="form-control" id="option2" aria-describedby="basic-addon3" readOnly value={ROOT_URL+query_str3} />
+					<input type="text" className="form-control link-option" id="option2" aria-describedby="basic-addon3" readOnly value={ROOT_URL+query_str3} />
+					<span className="input-group-btn"><a href={ROOT_URL+query_str3} target="_blank" className="btn btn-secondary">Try it</a></span>
 				</div>
 			</div>
 		)
@@ -46,7 +54,7 @@ class LinkOptions extends Component{
 
 function mapStateToProps(state)
 {
-	return {LRD:state.LRD,biz:state.biz};
+	return {LRD:state.LRD.value,biz:state.biz, isFetching:state.LRD.isFetching};
 }
 
 export default connect(mapStateToProps)(LinkOptions);

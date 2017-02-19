b06ee864fc18236c3575d53d9b79403fb42fc0b7
Leo Lee <nxl1969@gmail.com>
2017-02-19 02:08:30 -0500

    Pass the google place object to the action creator

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,23 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {getPlaceInfo} from '../actions/index';
+import {fetchLRD,selectBiz} from '../actions/index';
 
-export default class AutoComplete extends Component{
+class AutoComplete extends Component{
 	constructor(props){
 		super(props);
-		this.state = {place:''};
+		this.state = {biz_name:'',biz_address:''};
 		this.autocomplete = null;
 		this.onFormSubmit = this.onFormSubmit.bind(this);
-		this.onPlaceChange = this.onPlaceChange.bind(this);
+		//this.onPlaceChange = this.onPlaceChange.bind(this);
 	}
 	onFormSubmit(event){
 		event.preventDefault();
 		//this.props.getPlaceInfo(this.state.place);
-		if(this.state.place.trim()!= '')
+		const autocomplete_val = document.querySelector('#autocomplete_field').value;
+		if(autocomplete_val.trim()!= '')
 		{
 				const place = this.autocomplete.getPlace();
-				console.log(place.place_id);
+				//console.log(place);
+				//const biz_name = place.name;
+				//const biz_address = place.formatted_address;
+				//this.setState({biz_name:biz_name,biz_address:biz_address});
+				const place_cid = place.url.substr(place.url.indexOf("cid=") + 4);
+				this.props.fetchLRD(place_cid);
+				this.props.selectBiz(place);
 				if (!place.geometry) {
 						// User entered the name of a Place that was not suggested and
 						// pressed the Enter key, or the Place Details request failed.
@@ -31,20 +38,22 @@ export default class AutoComplete extends Component{
 		}
 		
 			
-		//this.setState({place:''});
+		
 	}
-	onPlaceChange(event){
+	/*onPlaceChange(event){
+	
+				
 		this.setState({place:event.target.value});
-	}
+	}*/
 	render(){
 		return (
 			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input id="autocomplete_field"  type="text" 
 						className="form-control" 
 						name="business_name" 
-						value={this.state.place} 
+						
 						placeholder="Type a business name here and select one from the drop down list"
-						onChange={this.onPlaceChange}/>
+						/>
 				<span className="input-group-btn">
 					<button className="btn btn-primary" type="submit">Generate</button>
 				</span>
@@ -72,3 +81,4 @@ export default class AutoComplete extends Component{
 		});*/
 	}
 }
+export default connect(null,{fetchLRD,selectBiz})(AutoComplete);

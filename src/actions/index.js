8c68b950b7923b3140021a92b77559826dd5e2aa
Leo Lee <nxl1969@gmail.com>
2017-02-19 16:58:09 -0500

    Add action creator setFetchingState It sets a fetching flag to true if the ajax call is still fetching the data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_LRD = 'FETCH_LRD';
 export const SELECT_BIZ = 'SELECT_BIZ';
+export const SET_FETCHING_STATE = 'SET_FETCHING_STATE';
 export function fetchLRD(place_cid){
 	const request = axios.get(`http://localhost:8888/php/getLRD.php?place_cid=${place_cid}`);
 	
@@ -14,5 +15,12 @@ export function selectBiz(biz){
 	return {
 		type: SELECT_BIZ,
 		payload:biz
+	}
+}
+export function setFetchingState(isFetching){
+	
+	return {
+		type:SET_FETCHING_STATE,
+		payload:isFetching
 	}
 }
